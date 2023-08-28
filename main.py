208e99159fb1a3648b1b31c0c66eca1cd200b77b
kgopu <kgopu@uncc.edu>
2023-08-28 13:04:05 -0400

    Version 2: handle files separately

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import Constants
 import csv
 from cymple import QueryBuilder
+import os
+import logging
 
 from GraphRepo import GraphRepo
 
@@ -16,76 +18,125 @@ def extract_data_from_csv(path):
     return rows
 
 
-def extract_left_right_relationship_info(data):
-    left_entity_info = {}
-    right_entity_info = {}
-    relationship_props = {}
-    for key, value in data.items():
-        split_key = key.split(Constants.SEPARATOR)
-        entity_type = split_key[0]
-        if entity_type == Constants.LEFT_ENTITY:
-            left_entity_info[split_key[2]] = value
-            left_entity_info[Constants.LABEL] = split_key[1]
-        elif entity_type == Constants.RIGHT_ENTITY:
-            right_entity_info[split_key[2]] = value
-            right_entity_info[Constants.LABEL] = split_key[1]
-        elif entity_type == Constants.RELATIONSHIP:
-            attribute = split_key[2] if len(split_key) == 3 else split_key[1]
-            relationship_props[attribute] = value
-    return left_entity_info, right_entity_info, relationship_props
-
-
-def is_id_exists(node_id, label):
+def is_id_exists(node_id, node_label):
     graph_repo = GraphRepo()
     qb = QueryBuilder()
-    query = qb.match().node(labels=label, ref_name='n', properties={Constants.ID: str(node_id)}).return_literal('n')
-    records, summary, keys = graph_repo.execute_query(query)
-    return len(records) > 0
+    query = qb.match().node(labels=node_label, ref_name='n', properties={Constants.ID: str(node_id)}).return_literal(
+        'n')
+    try:
+        records, summary, keys = graph_repo.execute_query(query)
+        return len(records) > 0
+    except Exception as ex:
+        logging.exception("exception, %s, occurred while checking the existence of node" %ex)
 
 
-def create_node(entity):
-    graph_repo = GraphRepo()
-    label = entity[Constants.LABEL]
-    entity.pop(Constants.LABEL, None)
-    node_id = entity[Constants.ID]
-    exists = is_id_exists(node_id, label)
-    if not exists:
-        qb = QueryBuilder()
-        query = qb.create().node(labels=label, properties=entity)
-        try:
-            print('executing query...')
-            graph_repo.execute_query(query)
-        except Exception as e:
-            print("an error occurred while creating a node" + e)
+def create_node(node_label, entity_rows):
+    for entity in entity_rows:
+        if not is_id_exists(entity[Constants.ID], node_label):
+            qb = QueryBuilder()
+            query = qb.create().node(labels=label, properties=entity)
+            logging.info("executing query %s" % query)
+            try:
+                graph_repo = GraphRepo()
+                graph_repo.execute_query(query)
+            except Exception as e:
+                logging.exception("An error, %s, occurred while creating a node." % e)
 
 
-def construct_kg(l_entity, r_entity, rel_entity):
-    l_label = l_entity[Constants.LABEL]
-    r_label = r_entity[Constants.LABEL]
-    create_node(l_entity)
-    create_node(r_entity)
+def create_relationship_helper(l_label, r_label, rel_entity):
     qb = QueryBuilder()
     query_match_left = qb.match() \
         .node(labels=l_label, ref_name='l') \
-        .where('l.' + Constants.ID, Constants.EQUALS, l_entity[Constants.ID])
+        .where('l.' + Constants.ID, Constants.EQUALS, rel_entity[Constants.SUBJECT])
     query_match_right = qb.match() \
         .node(labels=r_label, ref_name='r') \
-        .where('r.' + Constants.ID, Constants.EQUALS, r_entity[Constants.ID])
+        .where('r.' + Constants.ID, Constants.EQUALS, rel_entity[Constants.PREDICT])
     query_create_rel = qb.create() \
         .node(ref_name='l') \
-        .related_to(ref_name='rel', label=rel_entity['name']) \
+        .related_to(ref_name='rel', label=rel_entity[Constants.NAME]) \
         .node(ref_name='r')
     final_query = query_match_left + query_match_right + query_create_rel;
-    graph_repo = GraphRepo()
     try:
+        graph_repo = GraphRepo()
         graph_repo.execute_query(final_query)
     except Exception as e:
         print("an error occurred while creating relationship." + e)
 
 
+def get_label(name_split):
+    name_split = [name.capitalize() for name in name_split]
+    return '_'.join(name_split[1:])
+
+
+def parse_relation(relation_row):
+    relation = {}
+    for key, value in relation_row.items():
+        split_key = key.split(Constants.DOLLAR)
+        if split_key[0] == Constants.SUBJECT:
+            relation[Constants.SUBJECT] = value
+        elif split_key[0] == Constants.PREDICT:
+            relation[Constants.PREDICT] = value
+        elif split_key[1] == Constants.NAME:
+            relation[Constants.NAME] = str(value).upper()
+        elif split_key[1] == Constants.TYPE:
+            relation[str(split_key[2]).upper()] = value
+    return relation
+
+
+def check_relationship(relation, subject_label, predict_label):
+    if not is_id_exists(relation[Constants.SUBJECT], subject_label):
+        return False
+    elif not is_id_exists(relation[Constants.PREDICT], predict_label):
+        return False
+    else:
+        return True
+
+
+def create_relationship(relation_rows, subject_label, predict_label):
+    success = 0
+    failure = 0
+    for relation in relation_rows:
+        parsed_relation = parse_relation(relation)
+        can_form_relationship = check_relationship(parsed_relation, subject_label, predict_label)
+        if not can_form_relationship:
+            logging.info(
+                " either subject_id %s or object_id %s doesn't exist in database" % (subject_label, predict_label))
+            failure += 1
+        else:
+            try:
+                create_relationship_helper(subject_label, predict_label, parsed_relation)
+            except Exception as ex:
+                logging.exception(
+                    "exception  %s occurred while creating relationship between %s and %s" % (
+                    ex, subject_label, predict_label))
+                failure += 1
+            else:
+                success += 1
+    return success, failure
+
+
+def get_subject_predict_label(file_name):
+    split_name = file_name.split(Constants.HYPHEN)
+    labels = []
+    for name in split_name:
+        if str(name).startswith(Constants.ENTITY):
+            labels.append(get_label(name.split(Constants.UNDERSCORE)))
+    return labels[0], labels[1]
+
+
 if __name__ == '__main__':
-    formatted_data = extract_data_from_csv(Constants.FILE_PATH)
-    for each_row in formatted_data:
-        left_entity, right_entity, relationship_entity = extract_left_right_relationship_info(each_row)
-        construct_kg(left_entity, right_entity, relationship_entity)
-        
+    file_path = input('Enter the path of the file- do not enclose path in single/double quotes: ')
+    file_name = os.path.basename(file_path)
+    file_name, extension = file_name.split(Constants.DOT)
+    if str(file_name).startswith(Constants.ENTITY):
+        entity_rows = extract_data_from_csv(str(file_path))
+        file_name_split = file_name.split(Constants.UNDERSCORE)
+        label = get_label(file_name_split)
+        create_node(label, entity_rows)
+    elif str(file_name).startswith(Constants.RELATION):
+        relation_rows = extract_data_from_csv(str(file_path))
+        subject_label, predict_label = get_subject_predict_label(file_name)
+        success, failure = create_relationship(relation_rows, subject_label, predict_label)
+        print("relationship status : success = " + str(success) + " failure = " + str(failure))
+    else:
+        print("invalid file format")
